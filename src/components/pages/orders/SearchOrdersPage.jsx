ae48043262cfa7d5126a55f005020e9bca14cf47
unknown <fd13122019@gmail.com>
2025-07-04 21:27:05 -0500

    ajuste

--- src/components/pages/orders/SearchOrdersPage.jsx
+++ src/components/pages/orders/SearchOrdersPage.jsx
@@ -75,13 +75,7 @@ export default function SearchOrdersPage() {
                       <span className="text-xs text-blue-600 ml-2 animate-pulse">Generando PDF...</span>
                     )}
                     <MailButton onClick={() => handleSendEmail(o.ord_id)} />
-                    <PrintButton onClick={() => handlePrintOrder(o.ord_id)} disabled={printingOrderId === o.ord_id} />
-                    {printingOrderId === o.ord_id && (
-                      <svg className="animate-spin h-4 w-4 text-blue-600 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
-                      </svg>
-                    )}
+                    <PrintButton onClick={() => handlePrintOrder(o.ord_id)} disabled={printingOrderId === o.ord_id} />          
                   </div>
                 );
               }
